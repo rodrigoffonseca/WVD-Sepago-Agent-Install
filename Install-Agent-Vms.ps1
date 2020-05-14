822a5b0d99d9085a14e77fc43fd0f932927ea5fc
Rodrigo Fonseca <rodrigoffonseca@gmail.com>
2020-05-14 10:52:29 -0300

    Add files via upload

---
+++ Install-Agent-Vms.ps1
@@ -0,0 +1,10 @@
+﻿$rgname = "RG-Foca-Infra"
+#$scripturl = "https://safocainfra.blob.core.windows.net/wvd-agent/test-WVD-agent.ps1"
+#$scriptfile = "test-WVD-agent.ps1"
+$scriptfile = "deploy-WVD-agent.ps1"
+$scripturl = "https://safocainfra.blob.core.windows.net/wvd-agent/deploy-WVD-Agent.ps1"
+$location = "eastus"
+Connect-AzAccount
+Select-AzSubscription -Subscription "cce01445-8719-4563-b5b7-37b26250b020" -Scope CurrentUser -Force -Tenant 
+Set-AzContext -Subscription "cce01445-8719-4563-b5b7-37b26250b020"
+Get-AzVM -ResourceGroupName $rgname | %{Set-AzVMCustomScriptExtension -ResourceGroupName $rgname -VMName $_.Name -FileUri $scripturl -Name "Deploy-Agent" -Run $scriptfile -Location $location -NoWait} 
---
+++ deploy-WVD-Agent.ps1
@@ -0,0 +1,10 @@
+﻿#Deploy Sepago Monitoring Agent
+$sitePathzip = "C:\Program Files\zip"
+$output = "$sitePath\app.zip"
+$sitePath = "C:\Program Files"
+$url = "https://safocainfra.blob.core.windows.net/wvd-agent/ITPC-LogAnalyticsAgent.zip"
+New-Item -ItemType Directory $sitePathzip -Force
+(New-Object System.Net.WebClient).DownloadFile($url, $output)
+Expand-Archive -Path $output -DestinationPath $sitePath -Force
+cd 'C:\Program Files\ITPC-LogAnalyticsAgent'
+.\ITPC-LogAnalyticsAgent.exe -install
